f72cf4cd674649e244f365ecd4a8f1a6b1d4b46e
robfromerd <envirohtml@gmail.com>
2021-02-15 10:44:46 -0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,13 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
-import { IonPage, IonHeader, IonContent, IonToolbar, IonTitle, IonButtons, IonBackButton, IonButton, IonIcon, IonMenuButton, IonSearchbar, IonSegment, IonSegmentButton } from '@ionic/react';
-import { personCircle, search, helpCircle, star, create, ellipsisHorizontal, ellipsisVertical } from 'ionicons/icons';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-     <Ion-toolbar>
-        <Ion-buttons slot="start">
-          <Ion-menu-button auto-hide="false"></Ion-menu-button>
-        </Ion-buttons>
-        <Ion-buttons slot="primary">
-          <Ion-button>
-            <Ion-icon slot="icon-only" ios="ellipsis-horizontal" md="ellipsis-vertical"></Ion-icon>
-          </Ion-button>
-        </Ion-buttons>
-
-        <Ion-title>Standard</Ion-title>
-      </Ion-toolbar>
+        <IonToolbar>
+          <IonTitle>Ionic Blank Upated</IonTitle>
+        </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
         The world is your oyster.
